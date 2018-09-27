e4a9f57780d1f0bb639cb00b8a52bf780c9ba8d3
Aleksandr Zelenin <aleksandr@zelenin.me>
2018-09-27 03:36:25 +0300

    windows tags

--- client/tdjson_dynamic.go
+++ client/tdjson_dynamic.go
@@ -1,9 +1,11 @@
 // +build libtdjson
-// +build linux
+// +build linux windows
 
 package client
 
 /*
 #cgo linux LDFLAGS: -ltdjson -lstdc++ -lssl -lcrypto -ldl -lz -lm
+#cgo windows CFLAGS: -Ic:/td -Ic:/td/example/csharp/build
+#cgo windows LDFLAGS: -Lc:/td/example/csharp/build/Release -ltdjson
 */
 import "C"
